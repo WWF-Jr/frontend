5557b2dbd9215f1c84c3e0f2de2d7f0231c4777b
Nalin Angrish <nalinangrish2005@gmail.com>
2021-07-05 09:08:57 +0530

    Improved Dark theme and added home screen to app. Also added the 4k version of the earth background and set it as default

--- pages/app/run.js
+++ pages/app/run.js
@@ -22,6 +22,9 @@ export default class App extends React.Component{
 						<button onClick={()=>{this.setState({page:0})}}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z"/></svg>
 						</button>
+						<button onClick={()=>{window.history.back()}}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fillRule="evenodd" clipRule="evenodd"><path d="M11 21h8v-2l1-1v4h-9v2l-10-3v-18l10-3v2h9v5l-1-1v-3h-8v18zm10.053-9l-3.293-3.293.707-.707 4.5 4.5-4.5 4.5-.707-.707 3.293-3.293h-9.053v-1h9.053z"/></svg>
+						</button>
 					</div>
 				</div>
 			)
@@ -38,6 +41,19 @@ export default class App extends React.Component{
 
 
 function page1(comp){
+	return (
+		<section>
+			<h1 className="title">Welcome to The App!</h1>
+			<div className={styles.buttons}>
+				<button onClick={()=>{
+					comp.setState({page:comp.state.page+1})
+				}}>Start</button>
+			</div>
+		</section>
+	)
+}
+
+function page2(comp){
 	let regionButtons = []
 	for(let i=0;i<comp.regions.length;i++){
 		regionButtons.push(<button key={i} className={styles.button} onClick={()=>{
@@ -54,7 +70,7 @@ function page1(comp){
 	)
 }
 
-function page2(comp){
+function page3(comp){
 	return (
 		<section>
 			<h1 className="title">{comp.state.region}</h1>
@@ -64,7 +80,7 @@ function page2(comp){
 
 
 
-const pages = [page1, page2]
+const pages = [page1, page2, page3]
 export const getServerSideProps = async (ctx) => {
 	let regions = ["Asia", "North America", "South America", "Europe", "Africa", "Australia"] //(await new axios(`${ctx.protocol}://wwf-jr.herokuapp.com/fetch_cont`, {})).data
 
