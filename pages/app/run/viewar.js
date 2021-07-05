04308fe3b2b60d75acad53676d29895b14e1773d
Nalin Angrish <nalinangrish2005@gmail.com>
2021-07-05 21:02:23 +0530

    Add message to AR

--- pages/app/run/viewar.js
+++ pages/app/run/viewar.js
@@ -10,6 +10,6 @@ export default class GoTo3D extends React.Component {
 		return (<div></div>)
 	}
 	componentDidMount(){
-		document.location.assign(document.location.href.replace("wwf-jr.netlify.app/app/run/view3d", "wwf-3d.netlify.app"))
+		document.location.assign(document.location.href.replace("wwf-jr.netlify.app/app/run/view3d", "wwf-3d.netlify.app")+"&error=AR+Models+are+currently+not+supported")
 	}
 }
